b8ef72a4b773ccf396942156cc9c5b29f3f971e2
raon9401 <raon9401@naver.com>
2024-03-21 22:50:53 +0900

    fix: delete App component

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import AppClass from './AppClass';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <AppClass />
-
   </React.StrictMode>
 );
 
